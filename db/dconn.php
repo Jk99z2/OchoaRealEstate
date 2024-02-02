aa03c50d37ca81fda46046800176d72b3b348eda
Jk99z2 <eochoa11@ucol.mx>
2024-02-02 17:43:11 -0600

    Main commit

---
+++ db/dconn.php
@@ -0,0 +1,12 @@
+<?php
+$db_host = "localhost"; //localhost server 
+$db_user = "root";	//database username
+$db_password = "";	//database password   
+$db_name = "u584847502_inmobiliaria";	//database name
+try {
+	global $db;
+	$db = new PDO("mysql:host={$db_host};dbname={$db_name}", $db_user, $db_password);
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOEXCEPTION $e) {
+	$e->getMessage();
+}
